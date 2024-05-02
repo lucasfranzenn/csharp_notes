66b5e0b8123a4dda3a08e2a70941ac8e0719395d
Lucas Franzen <lucasfranzen@alunos.utfpr.edu.br>
2024-05-01 22:10:29 -0300

    :sparkles: feat: XML Serialization

---
+++ XML Serialization/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_Serialization
+{
+	[DataContract]
+    internal class Person
+    {
+		[DataMember]
+		private string? _name;
+
+		[DataMember]
+		private uint _age;
+
+        public string? Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		public uint Age
+		{
+			get { return _age; }
+			set { _age = value; }
+		}
+
+        public Person(string name, uint age)
+        {
+			_name = name;
+			_age = age;
+        }
+
+        public Person()
+        {
+            
+        }
+    }
+}
---
+++ XML Serialization/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml;
+
+namespace XML_Serialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //My Object to serialize
+        Person personOne = new("Lucas", 19);
+
+        #region Setting up DataContractSerializer
+        // Creating a XML DataContract of class Person
+        DataContractSerializer xmlSerializer = new DataContractSerializer(typeof(Person));
+
+        //My StringBuilder which will contain my XML text
+        StringBuilder personObject = new();
+        #endregion
+
+        #region Serialize
+        // Configurating XML Writer Settings
+        XmlWriterSettings xmlSettings = new XmlWriterSettings() { Indent = true };
+
+        //Serializing
+        using (XmlWriter xmlWriter = XmlWriter.Create(personObject, xmlSettings))
+        {
+            xmlSerializer.WriteObject(xmlWriter, personOne);
+        }
+        #endregion
+
+        #region Saving the content in a XML File
+
+        /*Creating my XML File*/
+        string path = @"person.xml";
+        FileStream myXml = File.Create(path);
+        myXml.Close();
+
+        /*Writing in my XML File*/
+        File.WriteAllText(path, personObject.ToString());
+
+        #endregion
+
+        #region Deserialize
+        //Creating my string to save XML Content
+        string xmlText = File.ReadAllText(path);
+        StringReader stringReader = new StringReader(xmlText);
+
+        //Deserializing
+        using (XmlReader xmlReader = XmlReader.Create(stringReader))
+        {
+            Person personDeserialized = (Person)xmlSerializer.ReadObject(xmlReader);
+        }
+        #endregion
+    }
+}
