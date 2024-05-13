73055dfef3b1331273863cfab39eb534b93edc05
Lucas Franzen <lucasfranzen@alunos.utfpr.edu.br>
2024-05-13 00:53:53 -0300

    :sparkles: feat: TCP Server and TCP Client

---
+++ Cliente Socket TCP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Cliente_Socket_TCP
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Creating TCP Client
+            TcpClient tcpClient = new TcpClient("localhost", 13);
+            NetworkStream ns = tcpClient.GetStream();
+
+            //Sending data to server
+            byte[] bufferSendToServer = Encoding.ASCII.GetBytes("Hello world from client");
+            ns.Write(bufferSendToServer, 0, bufferSendToServer.Length);
+
+            //Receiving data from server
+            byte[] bufferReceivedFromServer = new byte[256];
+            int bytesRead = ns.Read(bufferReceivedFromServer, 0, bufferReceivedFromServer.Length);
+            Console.WriteLine("Data received from server: " + Encoding.ASCII.GetString(bufferReceivedFromServer, 0, bufferReceivedFromServer.Length));
+
+            //Closing...
+            ns.Close();
+            tcpClient.Close();
+        }
+    }
+}
---
+++ Servidor Socket TCP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace Servidor_Socket_TVP
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            TcpListener tcpListener = new(IPAddress.Any, 13);
+            tcpListener.Start();
+
+            Console.WriteLine("Waiting connection...");
+
+            TcpClient client = tcpListener.AcceptTcpClient();
+
+            Console.WriteLine("Connection established...");
+
+            NetworkStream ns = client.GetStream();
+
+            try
+            {
+                //Receiving data from client
+                byte[] bufferFromClient = new byte[256];
+                int readBytes = ns.Read(bufferFromClient, 0, bufferFromClient.Length);
+                Console.WriteLine("Data received from client: " + Encoding.ASCII.GetString(bufferFromClient, 0, bufferFromClient.Length));
+
+                //Sending data to client
+                byte[] bufferToClient = Encoding.ASCII.GetBytes("Hello World from server!");
+                ns.Write(bufferToClient, 0, bufferToClient.Length);
+
+                ns.Close();
+                client.Close();
+                tcpListener.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
