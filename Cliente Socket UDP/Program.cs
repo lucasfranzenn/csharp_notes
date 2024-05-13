fbcd272411c184693791c4a2838b3ca30075607a
Lucas Franzen <lucasfranzen@alunos.utfpr.edu.br>
2024-05-13 20:40:37 -0300

    :sparkles: feat: UDP Server and UDP Client

---
+++ Cliente Socket UDP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Cliente_Socket_UDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000);
+            udpClient.Connect(endPoint);
+
+            byte[] bufferSendData = Encoding.ASCII.GetBytes("Sending data from Client to Server");
+            udpClient.Send(bufferSendData);
+
+            byte[] bufferReceivedData = new byte[256];
+            bufferReceivedData = udpClient.Receive(ref endPoint);
+            Console.WriteLine("Data received from server: " + Encoding.ASCII.GetString(bufferReceivedData));
+
+            udpClient.Close();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Servidor Socket UDP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Servidor_Socket_UDP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Creating UDP Server
+        int port = 11000;
+        UdpClient udpServer = new UdpClient(port);
+        IPEndPoint endPoint = new(IPAddress.Any, port);
+
+        //Receiving data from client
+        byte[] bufferFromClient = new byte[256];
+        bufferFromClient = udpServer.Receive(ref endPoint);
+        Console.WriteLine("Message received from client: " + Encoding.ASCII.GetString(bufferFromClient));
+
+        //Sending data from client
+        byte[] bufferToClient = Encoding.ASCII.GetBytes("Hello from UDP Server");
+        udpServer.Send(bufferToClient, bufferToClient.Length, endPoint);
+
+        udpServer.Close();
+
+        Console.ReadKey();
+
+    }
+}
