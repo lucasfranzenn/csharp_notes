5df2dc82fd4c787a4e8c5568d013efb7a6f3b134
Lucas Franzen <lucasfranzen@alunos.utfpr.edu.br>
2024-05-17 23:56:55 -0300

    :sparkles: feat: My first windows service

---
+++ My first windows service/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_first_windows_service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Diagnostics.Debugger.Launch(); // To debug a service
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
